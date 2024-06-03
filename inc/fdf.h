4519a80475e933c16386f62ac5387adc04ae9407
leoagea <leoagea@gmail.com>
2024-06-03 18:18:08 +0200

    Add : main.c and basic funct mlx

---
+++ inc/fdf.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 17:09:01 by lagea             #+#    #+#             */
+/*   Updated: 2024/06/03 18:15:13 by lagea            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+#define FDF_H
+
+#include "libft.h"
+#include "../mlx/mlx.h"
+
+# define ESC 53
+
+typedef struct s_mlx
+{
+	void *mlx;
+	void *win;
+}				t_mlx;
+
+typedef struct t_data
+{
+	t_mlx	mlx;
+}				t_data;
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 17:08:45 by lagea             #+#    #+#             */
+/*   Updated: 2024/06/03 18:15:40 by lagea            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/fdf.h"
+
+int destroy(t_data *data)
+{
+	mlx_destroy_window(data->mlx.mlx, data->mlx.win);
+	free(data->mlx.mlx);
+	exit(0);
+	return 0;
+}
+
+int keypress(int keysym, t_data *data)
+{
+	// printf("key : %d\n",keysym);
+	if (keysym == ESC)
+		destroy(data);
+	return 0;
+}
+
+int init_data(t_data *data)
+{
+	data = NULL;
+	return 0;
+}
+
+int main(int ac, char **av)
+{
+	(void) ac;
+	(void) av;
+	t_data data;
+
+	// init_data(&data);
+	data.mlx.mlx = mlx_init();
+	data.mlx.win = mlx_new_window(data.mlx.mlx, 1000, 1000, "FdF");
+	mlx_hook(data.mlx.win, 17, 0, &destroy, &data);
+	mlx_hook(data.mlx.win, 2, 0, &keypress, &data);
+	mlx_loop(data.mlx.mlx);
+
+	return 0;
+}
