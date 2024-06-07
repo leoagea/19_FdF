664ba073076962d8b053731e0fb4eaa6705b4cf5
leoagea <leoagea@gmail.com>
2024-06-07 17:51:02 +0200

    Update : accept change from merge

--- inc/fdf.h
+++ inc/fdf.h
@@ -6,11 +6,7 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 17:09:01 by lagea             #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2024/06/07 15:15:47 by lagea            ###   ########.fr       */
-=======
-/*   Updated: 2024/06/07 12:55:43 by lagea            ###   ########.fr       */
->>>>>>> b0335ae3deecd1212a680d12c6a46292846214b9
+/*   Updated: 2024/06/07 17:46:34 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +18,6 @@
 
 # define ESC 53
 
-<<<<<<< HEAD
-=======
-typedef struct s_col
-{
-	t_dll *dll;
-	struct s_col *next;
-}				t_col;
-
->>>>>>> b0335ae3deecd1212a680d12c6a46292846214b9
 typedef struct s_mlx
 {
 	void *mlx;
@@ -52,12 +39,8 @@ typedef struct t_data
 
 /*--------------------Init--------------------*/
 
-<<<<<<< HEAD
 int dll_init(t_dll *dll);
 t_dll	*dll_new(void);
-=======
-
->>>>>>> b0335ae3deecd1212a680d12c6a46292846214b9
 
 /*--------------------Parse-------------------*/
 
