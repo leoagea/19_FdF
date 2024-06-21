84d06fb4c2c61988772fc88cda276b0c02e07cd8
leoagea <leoagea@gmail.com>
2024-06-21 16:10:16 +0200

    Add : add funct put_image put puxil in the image then put image to the window

--- src/image.c
+++ src/image.c
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 18:14:17 by lagea             #+#    #+#             */
-/*   Updated: 2024/06/21 13:13:28 by lagea            ###   ########.fr       */
+/*   Updated: 2024/06/21 16:08:36 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void isometric_projection(t_data *data)
 {
-	data->map.zoom = (WIDTH / data->map.len_x) / 3;
 	int j;
 	int i = 0;
 	while (i < data->map.len_y)
@@ -27,7 +26,7 @@ void isometric_projection(t_data *data)
 			data->arr[i][j]->z *= data->map.zoom;
 			data->arr[i][j]->x_proj = (data->arr[i][j]->x - data->arr[i][j]->y) * cos(0.523599);
 			data->arr[i][j]->y_proj = (data->arr[i][j]->x + data->arr[i][j]->y) * cos(0.523599) - data->arr[i][j]->z;
-			data->arr[i][j]->x_proj += WIDTH / 2;
+			data->arr[i][j]->x_proj += (WIDTH) / 2;
 			data->arr[i][j]->y_proj += HEIGHT / 2;
 			// printf("i : %d, x : %d, y : %d , z : %d, color : %d\n", i, data->arr[i][j]->x_proj,data->arr[i][j]->y_proj, data->arr[i][j]->z, data->arr[i][j]->color);
 			j++;
@@ -38,15 +37,21 @@ void isometric_projection(t_data *data)
 
 void create_image(t_data *data)
 {
-	int i = 0;
-	int j;
-	if (!(data->image = mlx_new_image(data->mlx, WIDTH, HEIGHT)))
+	if (!(data->image = mlx_new_image(data->mlx, WIDTH - 300, HEIGHT)))
 	{
 		mlx_close_window(data->mlx);
 		error();
 	}
 	isometric_projection(data);
 	ft_memset(data->image->pixels, 16, data->image->width * data->image->height * sizeof(int32_t));
+	put_image(data);
+}
+
+void put_image(t_data *data)
+{
+	int i = 0;
+	int j;
+
 	while (i < data->map.len_y)
 	{
 		j = 0;
@@ -58,5 +63,5 @@ void create_image(t_data *data)
 		}
 		i++;
 	}
-	mlx_image_to_window(data->mlx, data->image, 0, 0);
+	mlx_image_to_window(data->mlx, data->image, 300, 0);
 }
