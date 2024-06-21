04ea7f8d379333f95aeb39220d9f6911fbc2ad60
leoagea <leoagea@gmail.com>
2024-06-21 17:02:21 +0200

    Done on 21/06 5:02 pm

--- inc/fdf.h
+++ inc/fdf.h
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 17:09:01 by lagea             #+#    #+#             */
-/*   Updated: 2024/06/18 18:54:42 by lagea            ###   ########.fr       */
+/*   Updated: 2024/06/21 16:51:50 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,13 +43,17 @@ typedef struct s_data
 	mlx_t	*mlx;
 	t_map	map;
 	t_point ***arr;
-	mlx_image_t* image;
+	mlx_image_t *menu;
+	mlx_image_t *image;
 }				t_data;
+
+void error(void);
 
 /*--------------------Init--------------------*/
 
 int dll_init(t_dll *dll);
 t_dll	*dll_new(void);
+void init_zoom(t_data *data);
 
 /*--------------------Parse-------------------*/
 
@@ -59,5 +63,23 @@ void	parse_file(char *path, t_data *data);
 
 void create_image(t_data *data);
 void isometric_projection(t_data *data);
+void put_image(t_data *data);
+
+/*--------------------Color--------------------*/
+
+int get_rgba(int r, int g, int b, int a);
+int get_r(int rgba);
+int get_g(int rgba);
+int get_b(int rgba);
+int get_a(int rgba);
+
+/*--------------------Menu---------------------*/
+
+void create_menu(t_data *data);
+
+/*--------------------Hooks--------------------*/
+
+void zoom(t_data *data, int value);
+void translation(t_data *data,bool dir, int value);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 17:08:45 by lagea             #+#    #+#             */
-/*   Updated: 2024/06/20 17:45:25 by lagea            ###   ########.fr       */
+/*   Updated: 2024/06/21 16:51:00 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,48 @@ int init_data(t_data *data)
 	return 0;
 }
 
-void hook(void* param)
+void hook(void *param)
 {
-	mlx_t* mlx = param;
+	t_data *data;
 
-	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
-		mlx_close_window(mlx);
+    data = (t_data *)param;
+	// mlx_t* mlx = param;
+
+	if (mlx_is_key_down(data->mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(data->mlx);
+	else if (mlx_is_key_down(data->mlx, MLX_KEY_UP)){
+		zoom(data, 5);
+		usleep(75000);
+		printf("Test UP\n");}
+	else if (mlx_is_key_down(data->mlx, MLX_KEY_DOWN)){
+		zoom(data, -5);
+		usleep(75000);
+		printf("Test DOWN\n");}
+}
+
+void	key_hook(mlx_key_data_t keydata, void* param)
+{
+	t_data *data;
+
+    data = (t_data *)param;
+	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
+		mlx_close_window(data->mlx);
+	else if (keydata.key == MLX_KEY_DOWN && keydata.action == MLX_PRESS){
+		zoom(data, -5);
+		printf("Test DOWN\n");}
+	else if (keydata.key == MLX_KEY_UP && keydata.action == MLX_PRESS){
+		zoom(data, 5);
+		printf("Test UP\n");}
+	else if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS){
+		translation(data, true, 5);
+		printf("Test W\n");}
+
+}
+
+void error(void)
+{
+	puts(mlx_strerror(mlx_errno));
+	exit(EXIT_FAILURE);
 }
 
 int main(int ac, char **av)
@@ -33,20 +69,20 @@ int main(int ac, char **av)
 	t_data data;
 
 	init_data(&data);
-	
 	if (ac == 2)
 		parse_file(av[1], &data);
 	else
 		exit(EXIT_FAILURE);
 
 	if (!(data.mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
-	{
-		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
-	}
+		error();
+	data.map.zoom = (WIDTH/ data.map.len_x) / 3;
+	printf("zoom : %d", data.map.zoom);
+	init_zoom(&data);
 	create_image(&data);
+	create_menu(&data);
 	
-	mlx_loop_hook(data.mlx, hook, data.mlx);
+	mlx_key_hook(data.mlx, &key_hook, &data);
 	
 	mlx_loop(data.mlx);
 	mlx_terminate(data.mlx);
