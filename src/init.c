c01440bbcba6b27519e136f35c5e8b3cd079e037
leoagea <leoagea@gmail.com>
2024-06-21 17:01:38 +0200

    Add : init_zoom funct

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 13:41:40 by lagea             #+#    #+#             */
-/*   Updated: 2024/06/07 17:42:34 by lagea            ###   ########.fr       */
+/*   Updated: 2024/06/21 16:51:31 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,22 @@ t_dll	*dll_new(void)
 	new->head = NULL;
 	new->tail = NULL;
 	return (new);
+}
+
+void init_zoom(t_data *data)
+{
+	int j;
+	int i = 0;
+	while (i < data->map.len_y)
+	{
+		j = 0;
+		while (j < data->map.len_x)
+		{
+			data->arr[i][j]->x *= data->map.zoom;
+			data->arr[i][j]->y *= data->map.zoom;
+			data->arr[i][j]->z *= data->map.zoom;
+			j++;
+		}
+		i++;
+	}
 }
